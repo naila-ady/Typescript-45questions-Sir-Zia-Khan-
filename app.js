8f31a1a68dab121282078fc269de717ad3913dbb
naila-ady <164941279+naila-ady@users.noreply.github.com>
2024-03-30 12:51:04 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-var firstName = "Eric";
-console.log("\"Hello ".concat(firstName, " ,would you like to learn some python today?\""));
+//saves the name
+var personName = "Mustafa";
+//print the name in small letters
+console.log(personName.toLowerCase());
+//print the name in capital letters
+console.log(personName.toUpperCase());
+//print the name with first capital and other small letters
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
--- app.ts
+++ app.ts
@@ -1,2 +1,15 @@
-let firstName:string="Eric"
-console.log(`"Hello ${firstName} ,would you like to learn some python today?"`);
+//saves the name
+let personName :string ="Mustafa";
+
+//print the name in small letters
+console.log(personName.toLowerCase());
+//print the name in capital letters
+console.log(personName.toUpperCase());
+//print the name with first capital and other small letters
+console.log(personName.charAt(0).toUpperCase()+personName.slice(1).toLowerCase());
+
+
+
+
+
+
